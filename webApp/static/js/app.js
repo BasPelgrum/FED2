e840367d94c83089768fc384c099e63d015a8a5d
BasPelgrum <bas_pelgrum@hotmail.com>
2014-10-01 16:03:31 +0200

    DT1- Update
    Images toevoegen

--- webApp/static/js/app.js
+++ webApp/static/js/app.js
@@ -1,13 +1,13 @@
-var movieApp = movieApp || {}; 
+var movieApp = movieApp || {};
 
 
 //self invoking anonymous function
 (function(){
 
-/* CONTROLLER
-	- Object literal.
-	- Method: "init" 
-*/
+	/* CONTROLLER
+		- Object literal.
+		- Method: "init" 
+	*/
 	var controller = {
 		init:function () {
 			router.init();
@@ -16,18 +16,19 @@ var movieApp = movieApp || {};
 	};
 
 
-/* ROUTER
-	- Object literal.
-	- Method: "init".
-	- Properties: "about" en "movies" 
-*/
+	/* ROUTER
+		- Object literal.
+		- Method: "init".
+		- Properties: "about" en "movies" 
+	*/
 	var router = {
 		init: function () {
 			routie({
-				// click about: execute function
+				// click about: execute function (show class about, hide class movies)
 	    		'about': function() {
 	    			sections.toggle(".about", ".movies");
 	    		},
+	    		// Click movies: execute function. 
 	    		'movies': function() {
 	    			sections.toggle(".movies", ".about");
 	    		}
@@ -36,20 +37,21 @@ var movieApp = movieApp || {};
 	};
 
 
+
 	var sections = {
 		init: function(){
-			//this refereert naar het element waar het in staat. (sections.about of sections.movies)
+			//this refereert naar het element waar het in staat. (het sections object) -- Joost
 			this.about();
 			this.movies();
 		},
 
 		about: function(){
 			//voor de komma, waar de de content in moet. Na de komma, is de content.
-			Transparency.render(document.querySelector('.about'), movieApp.content.about);
+			Transparency.render(document.querySelector('.about'), movieApp.content.about, movieApp.content.directives);
 		},
 		
 		movies: function(){
-			Transparency.render(document.querySelector('.movies'), movieApp.content.movies);
+			Transparency.render(document.querySelector('.movies'), movieApp.content.movies, movieApp.content.directives);
 		},
 		
 		toggle: function(show, hide){
@@ -57,9 +59,11 @@ var movieApp = movieApp || {};
 			document.querySelector(hide).classList.remove('active');
 		} 
 	};
+	
 	//initialize controler
 	controller.init();
 }());
+
 
 //method: manier om iets te doen dus -> functie
 //propertie: eigenschap 
